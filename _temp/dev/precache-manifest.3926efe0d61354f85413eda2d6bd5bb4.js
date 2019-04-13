57be8da7787dc95c90bf12732a7aa9ca71c60733
Martin Bednář <martin.bednar33@gmail.com>
2019-04-13 16:35:25 +0200

    Initial commit
    A historical moment

---
+++ _temp/dev/precache-manifest.3926efe0d61354f85413eda2d6bd5bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "19d02b2c51de8da3a07d",
+    "url": "./static/js/main.19d02b2c.chunk.js"
+  },
+  {
+    "revision": "42d94ba35524d729e806",
+    "url": "./static/js/2.42d94ba3.chunk.js"
+  },
+  {
+    "revision": "19d02b2c51de8da3a07d",
+    "url": "./static/css/main.de484873.chunk.css"
+  },
+  {
+    "revision": "37cc1cefffce05a7d757056be76d068f",
+    "url": "./index.html"
+  }
+];
