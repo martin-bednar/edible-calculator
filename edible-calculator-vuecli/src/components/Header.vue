962488c93c052d0ebb94e148a67d435582696954
Martin Bednář <martin.bednar33@gmail.com>
2019-05-02 14:23:27 +0200

    Style changes
    Various design improvements

--- edible-calculator-vuecli/src/components/Header.vue
+++ edible-calculator-vuecli/src/components/Header.vue
@@ -2,7 +2,7 @@
     <div class="site-header__wrapper">
     <header class="site-header">
       <router-link to="/">
-       <h2> edible calculator <span class="blink"> _</span></h2>
+       <h2 class="site-header__main-heading"> edible calculator <span class="blink"> _</span></h2>
        </router-link>
 
     </header>
@@ -28,15 +28,18 @@ export default {
 
 <style lang="scss" scoped>
 .site-header{
-  padding: 2em 1em;
-    background-color: black;
+  padding: 1vh 0;
+    background-color: #192721;
     color:white;
   z-index: 99;
+}
+.site-header__main-heading{
+  font-size: 2.7em;
 }
 h2{
     padding: 0 .5em;
     margin: 0;
-    color:#0caa63;
+    color:#32e28a;
     text-align: center;
 }
 .site-header__nav-item{
@@ -54,12 +57,12 @@ h2{
 .site-header__nav {
 
   box-sizing: border-box;
-  background-color: #0caa63;
+  background-color: #1bc570;
   height: unset;
 
   a {
       font-size: 1.6em;
-      color: rgb(46, 43, 46);
+      color: rgb(15, 14, 15);
     &:hover,
     &.router-link-exact-active{
       color: white;
